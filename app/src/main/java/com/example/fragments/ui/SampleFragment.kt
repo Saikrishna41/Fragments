3590ae33d65cce7fb993a72999255002628981b9
Saikrishna41 <saai.krrishna@gmail.com>
2023-04-16 22:10:34 +0800

    statically added fragments

---
+++ app/src/main/java/com/example/fragments/ui/SampleFragment.kt
@@ -0,0 +1,23 @@
+package com.example.fragments.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragments.R
+
+class SampleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_sample,container,false)
+
+        return view
+    }
+
+
+}
